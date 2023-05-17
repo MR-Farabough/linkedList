68ce1a713d3cbff1351914e3f0708e06cb0d8332
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-05-17 17:33:20 -0500

    Created class for the list

---
+++ index.js
@@ -0,0 +1,13 @@
+class Node {
+    constructor(data, pointer = null) {
+        this.data = data;
+        this.pointer = pointer;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0
+    }
+}

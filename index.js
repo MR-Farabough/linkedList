95d2fbd5d307534dbb0e766e89c9c8517e0150de
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-05-17 17:58:43 -0500

    Added insertFirst() method

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ class Node {
 class LinkedList {
     constructor() {
         this.head = null;
-        this.size = 0
+        this.size = 0;
+    }
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+        this.size++
     }
 }
+
+const ll = new LinkedList();
+ll.insertFirst(100);
+ll.insertFirst(50)
+console.log(ll);
